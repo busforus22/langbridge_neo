b8578cbf49fd85954efaf4e0c21267ed90dc188c
ShioriHasegawa2629 <shasegawa995@gmail.com>
2026-05-12 22:50:54 +0900

    trust proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        //すべてのプロキシを信頼する
+        $middleware->trustProxies(at:'*');
+        
         // ミドルウェアのエイリアス登録 
         $middleware->alias([ 
             'is_admin' => IsAdmin::class, 
